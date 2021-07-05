5112ac0e1dc243ef88c63ef7a36fa1f1f5d8ad25
Nishi Agrawal <nishihere19@gmail.com>
2021-07-05 22:48:13 +0530

    Modified verification page, removed sign up button
    Signed-off-by: Nishi Agrawal <nishihere19@gmail.com>

--- src/routes/VerifyEmail.svelte
+++ src/routes/VerifyEmail.svelte
@@ -6,49 +6,8 @@
 </style>
 
 <script lang="ts">
-  import { toasts } from 'svelte-toasts';
-  import { navigate } from 'svelte-routing';
-  import config from '../../env';
-  import { Button } from 'svelte-materialify';
-  import { getContext } from 'svelte';
-  import { axiosInstance } from 'src/utils/axios';
-
-  let { theme } = getContext('theme');
   const parameter = new URLSearchParams(window.location.search);
   const webmailId = parameter.get('webmailId');
-  function signUp() {
-    axiosInstance({
-      method: 'get',
-      url: `${config.backendurl}/auth/check-verification`
-    })
-      .then(result => {
-        navigate('/registerdetails', { replace: true });
-        toasts.add({
-          title: 'Success',
-          description: `${result.data.message}`,
-          duration: 10000, // 0 or negative to avoid auto-remove
-          placement: 'bottom-right',
-          type: 'success',
-          showProgress: true,
-          theme: $theme.name
-        });
-      })
-      .catch(error => {
-        navigate('/error', { replace: true });
-        toasts.add({
-          title: 'Oops',
-          description:
-            error.response.data.message ||
-            error.response.data.errors[0].msg ||
-            'Something went wrong, please try again!',
-          duration: 10000, // 0 or negative to avoid auto-remove
-          placement: 'bottom-right',
-          type: 'error',
-          showProgress: true,
-          theme: $theme.name
-        });
-      });
-  }
 </script>
 
 <main>
@@ -58,6 +17,5 @@
       Kindly check <span class="webmailId">{webmailId}</span> for the confirmation link.<br
       /> You need to confirm your webmail account in order to complete the signing up process.
     </p>
-    <Button Tile id="signUp_button" on:click={signUp}>Sign Up</Button>
   </div>
 </main>
