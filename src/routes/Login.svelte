32be8841112e2b1a8dafc7ddfe4ad7ed7bbebf99
Sindhu <yellasindhu09@gmail.com>
2021-09-01 15:19:10 +0530

    [Forgot password]: functionality

--- src/routes/Login.svelte
+++ src/routes/Login.svelte
@@ -133,6 +133,7 @@
       <div class="registerContainer">
         <button class="submit_button" type="submit" on:click={verify}>Login</button>
         <div class="navigateOption">
+          <Link to="/forgotPassword" class="navigateLink">Forgot password?</Link>
           <p>Don't have an account?</p>
           <Link to="/register" class="navigateLink">Register Now</Link>
         </div>
